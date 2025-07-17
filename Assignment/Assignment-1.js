e7a3f6628b26dd8f3835f1039e50d620cba92f50
Naveenkumar506 <yanamandalanaveenkumar@gmail.com>
2025-07-17 14:34:47 +0530

    Client-side-JS-Form

--- Assignment/Assignment-1.js
+++ Assignment/Assignment-1.js
@@ -1,4 +1,4 @@
-//1.Write a program to check if a number is divisible by 7 or not? let number = 7;
+//21.Write a program to check if a number is divisible by 7 or not? let number = 7;
 
 let number = 7;
 if (number % 7 === 0) {
@@ -7,7 +7,7 @@ if (number % 7 === 0) {
   console.log("It is not adivisiable");
 }
 
-//  2.Write a program to check if a number is multiple of 3 or not?
+// 22.Write a program to check if a number is multiple of 3 or not?
 
 let multiple = 9;
 if (multiple % 3 === 0) {
@@ -16,7 +16,7 @@ if (multiple % 3 === 0) {
   console.log("not multiple  3");
 }
 
-// 3.Write a program to check if a number is positive or not?
+// 23.Write a program to check if a number is positive or not?
 
 const num = 10;
 if (num > 0) {
@@ -25,7 +25,7 @@ if (num > 0) {
   console.log("this is not positive number");
 }
 
-//4.Write a program to check if a number is 3-digit numbers or not?
+//24.Write a program to check if a number is 3-digit numbers or not?
 let threedigit = 201;
 if (threedigit >= 100 && threedigit <= 999) {
   console.log("It is 3-digit number");
@@ -33,7 +33,7 @@ if (threedigit >= 100 && threedigit <= 999) {
   console.log("It is not  3-digit number");
 }
 
-//5.Write a program to print even or odd for a given number?
+//25.Write a program to print even or odd for a given number?
 
 let numbers = 6;
 
@@ -43,7 +43,7 @@ if (numbers % 2 === 0) {
   console.log("it is odd number");
 }
 
-//6. Write a program to print the greatest number in given two numbers?
+//26. Write a program to print the greatest number in given two numbers?
 
 var num1 = 5;
 
@@ -57,7 +57,7 @@ if (num1 > num2) {
   console.log(num2);
 }
 
-// 7. Write a program to print the least number given two numbers?
+// 27. Write a program to print the least number given two numbers?
 
 let n1 = 10;
 let n2 = 30;
@@ -71,8 +71,9 @@ if (n1 <= n2 && n1 <= n3) {
   console.log(n3);
 }
 
-//
-let num1 = 25;
+//28 Write a program to print the greatest number in given three numbers?
+
+/* let num1 = 25;
 let num2 = 10;
 let num3 = 45;
 
@@ -84,7 +85,31 @@ if (num1 <= num2 && num1 <= num3) {
 } else {
   least = num3;
 }
-
 console.log("The least number is:", least);
 
+ */
 
+//29 Write a program to print the least number in given three numbers?
+
+//30.Write a program to print the given 3 numbers in ascending orders?
+let student_id = [100, 10, 55, 65, 20, 85];
+
+student_id.sort((a, b) => a - b);
+console.log(student_id);
+
+//31 Write a program to print the given 3 numbers in descending orders?
+let emp_id = [100, 10, 55, 65, 20, 85];
+
+emp_id.sort((a, b) => b - a);
+console.log(emp_id);
+
+console.log("fhghghghg");
+let a = 100;
+let b = 50;
+let c = 10;
+
+// let list = [88, 77, 66, 55, 66, 77];
+
+// let newlist = [...new Set(list)];
+// // let newlist = new Set(list);
+// console.log(newlist);
--- forloop/eight.js
+++ forloop/eight.js
@@ -1,25 +1,21 @@
 //8
 
+let num = 1;
+
+for (num = 1; num <= 10; num++) {
+  console.log(7, "x", num, "=", num * 7);
+}
+
+for (a = 1; a <= 10; a++) {
+  if (a % 2 === 0) {
+    console.log("five even numbers", a);
+  }
+}
+
+//while
+
 let i = 1;
 while (i <= 10) {
   console.log(7, "x", i, "=", i * 7);
   i++;
 }
-
-for (i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
-
-let i = 1;
-
-do {
-  console.log("hai....");
-
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-
-  i++;
-} while (i <= 10);
